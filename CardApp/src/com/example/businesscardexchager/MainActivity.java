116dd376678da86a6360bfc54c5fa0b6fa282019
Hoeks, Maikel <maikelhoeks@hotmail.com>
2014-10-03 11:19:52 +0200

    Actionbar + tabs

--- CardApp/src/com/example/businesscardexchager/MainActivity.java
+++ CardApp/src/com/example/businesscardexchager/MainActivity.java
@@ -1,15 +1,133 @@
 package com.example.businesscardexchager;
 
-import android.app.Activity;
+import android.app.ActionBar;
+import android.app.FragmentManager;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuInflater;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements TabListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	ViewPager viewPager;
+	ActionBar actionBar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		viewPager.setAdapter(new MyAdapter(getSupportFragmentManager()));
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+			@Override
+			public void onPageSelected(int arg0) {
+				// TODO Auto-generated method stub
+				actionBar.setSelectedNavigationItem(arg0);
+				// Log.d("EDR", "onPageSelected at " + " position " + arg0);
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+				// Log.d("EDR", "onPageScrolled at " + " position " + arg0 +
+				// " from " + arg1+ " with number of pixels " + arg2);
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+				/*
+				 * if(arg0==ViewPager.SCROLL_STATE_IDLE) { Log.d("EDR",
+				 * "onPageScrollStateChanged Idle"); }
+				 * if(arg0==ViewPager.SCROLL_STATE_DRAGGING) { Log.d("EDR",
+				 * "onPageScrollStateChanged Dragging"); }
+				 * if(arg0==ViewPager.SCROLL_STATE_SETTLING) { Log.d("EDR",
+				 * "onPageScrollStateChanged Settling"); }
+				 */
+
+			}
+		});
+		actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		ActionBar.Tab tab1 = actionBar.newTab();
+		tab1.setText("Collection");
+		tab1.setTabListener(this);
+
+		ActionBar.Tab tab2 = actionBar.newTab();
+		tab2.setText("My card");
+		tab2.setTabListener(this);
+
+		actionBar.addTab(tab1);
+		actionBar.addTab(tab2);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		// Log.d("EDR", "onTabReselected at " + " position " + tab.getPosition()
+		// + " name " + tab.getText());
+
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		// Log.d("EDR", "onTabSelected at " + " position " + tab.getPosition()
+		// + " name " + tab.getText());
+		viewPager.setCurrentItem(tab.getPosition());
+
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		// Log.d("EDR", "onTabUnselected at " + " position " + tab.getPosition()
+		// + " name " + tab.getText());
+	}
+}
+
+class MyAdapter extends FragmentPagerAdapter {
+	public MyAdapter(android.support.v4.app.FragmentManager fragmentManager) {
+		super(fragmentManager);
+	}
+
+	@Override
+	public Fragment getItem(int arg0) {
+		// TODO Auto-generated method stub
+		Fragment fragment = null;
+		if (arg0 == 0) {
+			fragment = new FragmentA();
+		}
+		if (arg0 == 1) {
+			fragment = new FragmentB();
+
+		}
+		return fragment;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 2;
+	}
+
+
 }
