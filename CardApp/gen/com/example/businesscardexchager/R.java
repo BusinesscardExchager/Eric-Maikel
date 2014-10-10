becd6942ef4522e4324bbbc32e222f20478629c2
Hoeks, Maikel <maikelhoeks@hotmail.com>
2014-10-10 08:41:24 +0200

    nieuwe icoontje

--- CardApp/gen/com/example/businesscardexchager/R.java
+++ CardApp/gen/com/example/businesscardexchager/R.java
@@ -224,13 +224,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_action_forward=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_action_share=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
+        public static final int ic_action_overflow=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_action_share=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
         public static final int action_search=0x7f080001;
         public static final int action_send=0x7f080002;
+        public static final int action_settings=0x7f080003;
         public static final int pager=0x7f080000;
     }
     public static final class layout {
@@ -243,16 +245,18 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int action_overflow=0x7f050009;
         public static final int action_search=0x7f050007;
         public static final int action_send=0x7f050008;
-        public static final int adres=0x7f05000a;
+        public static final int action_settings=0x7f05000c;
+        public static final int adres=0x7f05000b;
         public static final int app_name=0x7f050000;
         public static final int fragmentA=0x7f050004;
         public static final int fragmentB=0x7f050005;
         public static final int fragmentC=0x7f050006;
         public static final int hello_blank_fragment=0x7f050003;
         public static final int hello_world=0x7f050001;
-        public static final int naam=0x7f050009;
+        public static final int naam=0x7f05000a;
         public static final int title_activity_fragment_tab1=0x7f050002;
     }
     public static final class style {
