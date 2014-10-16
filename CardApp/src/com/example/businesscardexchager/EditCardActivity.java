54fca4fb610089c646fb6ce27dbca9cbc38aca98
Hoeks, Maikel <maikelhoeks@hotmail.com>
2014-10-16 16:02:01 +0200

    card

--- CardApp/src/com/example/businesscardexchager/EditCardActivity.java
+++ CardApp/src/com/example/businesscardexchager/EditCardActivity.java
@@ -1,19 +1,32 @@
 package com.example.businesscardexchager;
 
+import com.example.businesscardexchager.R.id;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 public class EditCardActivity extends Activity {
 
 	SharedPreferences sharedprefs;
 	public static final String MY_PREFERENCES = "MyPrefs";
 
+	@Override
+	public void onResume() 
+	{
+		super.onResume();
+		EditText et = (EditText) findViewById(id.naamContact);
+		if(sharedprefs.contains("naamCard"))
+		{
+			et.setText(sharedprefs.getString("naamCard", "Naam"));
+		}
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -40,5 +53,13 @@ public class EditCardActivity extends Activity {
 
 			}
 
+	}
+	public void SaveMyCard(View view) {
+		sharedprefs = getSharedPreferences(MY_PREFERENCES, Context.MODE_PRIVATE);
+		Editor editor = sharedprefs.edit();
+		EditText et = (EditText) findViewById(id.naamContact);
+		editor.putString("naamCard", et.getText().toString());
+		editor.commit();
+		finish();
 	}
 }
