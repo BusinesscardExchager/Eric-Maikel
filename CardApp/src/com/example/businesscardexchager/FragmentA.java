eaac2945a4a7b07326fbed0327c01fcc20fa65e5
EricDeRegter <ericderegter@gmail.com>
2014-10-16 16:09:51 +0200

    fragment a

---
+++ CardApp/src/com/example/businesscardexchager/FragmentA.java
@@ -0,0 +1,70 @@
+package com.example.businesscardexchager;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * 
+ */
+public class FragmentA extends Fragment {
+
+	public static final String MY_PREFERENCES = "MyPrefs";
+	SharedPreferences sharedprefs;
+	int count = 0;
+
+	public FragmentA() {
+		// Required empty public constructor
+		
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+	}
+
+	@Override
+	public void onResume() {
+		View view = getView().findViewById(R.id.fragmentA);
+		super.onResume();
+		Log.d("EDR", "resume");
+		if (sharedprefs != null) {
+			Log.d("EDR", "resume1");
+			if (sharedprefs.contains("achtergrondkleur")) {
+				Log.d("EDR", "resume1");
+				String color = sharedprefs.getString("achtergrondkleur", "");
+				Log.d("EDR", "resume2");
+				if (color.equals("Rood")) {
+					view.setBackgroundColor(getResources()
+							.getColor(R.color.red));
+				} else if (color.equals("Blauw")) {
+					view.setBackgroundColor(getResources().getColor(
+							R.color.blue));
+				} else if (color.equals("Groen")) {
+					view.setBackgroundColor(getResources().getColor(
+							R.color.green));
+				}
+			}
+		} else {
+			Log.d("EDR", "hoi");
+		}
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		sharedprefs = this.getActivity().getSharedPreferences(MY_PREFERENCES,Context.MODE_PRIVATE);
+		return inflater.inflate(R.layout.fragment_a, container, false);
+
+	}
+
+}
