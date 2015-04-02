9d2881f6f5ca55a2123411fe6147c7feda21f961
MaikelHoeks <maikelhoeks@hotmail.com>
2015-04-02 15:19:43 +0200

    test app voor buttons in cell

---
+++ IOS/test/test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Fhict on 02/04/15.
+//  Copyright (c) 2015 Fontys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
