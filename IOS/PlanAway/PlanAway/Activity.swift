ba37f8b08dc0f6c509155644dd25bcafee35585c
EricDeRegter <ericderegter@gmail.com>
2015-04-12 21:02:37 +0200

    logboek update

--- IOS/PlanAway/PlanAway/Activity.swift
+++ IOS/PlanAway/PlanAway/Activity.swift
@@ -9,15 +9,38 @@
 import UIKit
 
 class Activity: NSObject {
-   var name = ""
-    var detail = ""
+    var name: String?
+    var company: String?
+    var detail: String?
     var image: UIImage
-    var datum: NSDate?
+    var date: String?
+    var place: String?
+    var sitelink: String?
+    var trailerlink: String?
+    var aftermovie: String?
     
     init(Name name:String, Detail detail:String, Image image:String)
     {
         self.name = name
         self.detail = detail
         self.image = UIImage(named: image)!
+    }
+    
+    init(Name name:String, Company company:String, Detail detail:String, Image image:String, Date date:String, Place place:String, SiteURL sitelink:String, TrailerURL trailerlink:String, AftermovieURL aftermovie:String)
+    {
+        self.name = name
+        self.company = company
+        self.detail = detail
+        self.image = UIImage(named: image)!
+        self.date = date
+        self.place = place
+        self.sitelink = sitelink
+        self.trailerlink = trailerlink
+        self.aftermovie = aftermovie
+    }
+    
+    func toString()
+    {
+        println("Name: \(name) Company: \(company), Detail: \(detail), Date: \(date), Place: \(place), Site: \(sitelink), Trailer: \(trailerlink), Aftermovie: \(aftermovie)")
     }
 }
